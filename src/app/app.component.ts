f4c548f88638b813d05f5d7d71d7a29572eacb91
Admin <praveen.trainer@outlook.com>
2023-11-18 22:14:30 +0530

    fetch api data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,18 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent {
 
+
+  _http !: HttpClient ;
   constructor(http: HttpClient){
       // https://jsonplaceholder.typicode.com/users
+      this._http = http;
   }
 
   title = 'music-store-stanalone';
+  users!: Observable<any[]>;
 
-
-  users = []
+  ngOnInit(){
+    this.users = this._http.get<any[]>("https://jsonplaceholder.typicode.com/users")
+  }
+  
 }
